469749d99e87775706ce53e0814f6b4013cd7a3f
nqbefgvs <389179622@qq.com>
2021-05-21 13:56:13 +0800

    chapter 6 completed

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -79,3 +79,21 @@ class NewVisitorTest(LiveServerTestCase):
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
         self.fail('Finish the test!')
 
+    def test_can_start_a_list_for_one_user(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # The page updates again, and now shows both items on her list
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith starts a new to-do list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # She notices that her list has a unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
