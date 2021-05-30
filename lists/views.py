3f57650738b5760fcd4ef6bdeb34993764b5407a
nqbefgvs <389179622@qq.com>
2021-05-30 17:09:27 +0800

    chapter 7 p 51 passed

--- lists/views.py
+++ lists/views.py
@@ -13,12 +13,10 @@ def view_list(request):
 
 
 def home_page(request):
-
-    if request.method == 'POST':
-        Item.objects.create(text = request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
-
-    items = Item.objects.all()
     return render(request, 'home.html')
 
+
+def new_list(request):
+    Item.objects.create(text = request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
 
