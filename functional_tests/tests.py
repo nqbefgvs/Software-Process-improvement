d32c240d0734bb81fc6ddcc69a3d25e88fffefb9
nqbefgvs <389179622@qq.com>
2021-06-04 13:49:37 +0800

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -31,44 +31,63 @@ class NewVisitorTest(LiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
-    
-    def test_can_start_a_list_for_one_user(self):
-        # Edith has heard about a cool new online to-do app. She goes
-        # to check out its homepage
+
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        print('layout point 1')
         self.browser.get(self.live_server_url)
+        print('layout point 2')
+        self.browser.set_window_size(1024, 768)
 
-        # She noties the page title and header mention to-do lists
-        self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
-        self.assertIn('To-Do', header_text)
-
-        # She is invited to enter a to-do item straight away
+        # She notices the input box is nicely centered
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-            )
-
-        # She types "Buy peacock feathers" into a text box (Edith's hobby
-        # is tying fly-fishing lures)
-        inputbox.send_keys('Buy peacock feathers')
-
-        # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys('testing')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
-
-        # There is still a text box inviting her to add another item. She
-        # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        self.wait_for_row_in_list_table('1: testing')
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Use peacock feathers to make a fly')
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta = 10
+        )
+    
+    # def test_can_start_a_list_for_one_user(self):
+    #     # Edith has heard about a cool new online to-do app. She goes
+    #     # to check out its homepage
+    #     self.browser.get(self.live_server_url)
 
-        # The page updates again, and now shows both items on her list
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
-        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+    #     # She noties the page title and header mention to-do lists
+    #     self.assertIn('To-Do', self.browser.title)
+    #     header_text = self.browser.find_element_by_tag_name('h1').text
+    #     self.assertIn('To-Do', header_text)
+
+    #     # She is invited to enter a to-do item straight away
+    #     inputbox = self.browser.find_element_by_id('id_new_item')
+    #     self.assertEqual(
+    #         inputbox.get_attribute('placeholder'),
+    #         'Enter a to-do item'
+    #         )
+
+    #     # She types "Buy peacock feathers" into a text box (Edith's hobby
+    #     # is tying fly-fishing lures)
+    #     inputbox.send_keys('Buy peacock feathers')
+
+    #     # When she hits enter, the page updates, and now the page lists
+    #     # "1: Buy peacock feathers" as an item in a to-do list
+    #     inputbox.send_keys(Keys.ENTER)
+    #     time.sleep(1)
+    #     self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+    #     # There is still a text box inviting her to add another item. She
+    #     # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+    #     inputbox = self.browser.find_element_by_id('id_new_item')
+    #     inputbox.send_keys('Use peacock feathers to make a fly')
+    #     inputbox.send_keys(Keys.ENTER)
+    #     time.sleep(1)
+
+    #     # The page updates again, and now shows both items on her list
+    #     self.wait_for_row_in_list_table('1: Buy peacock feathers')
+    #     self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Satisfied, She goes back to sleep
 
